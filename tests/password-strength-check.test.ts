35e57719c1562585e19cf7397b0543032057218b
saymabushra <saymabushra@gmail.com>
2024-10-13 23:57:30 +0300

    added tet case for week,moderate and strong password check

--- tests/password-strength-check.test.ts
+++ tests/password-strength-check.test.ts
@@ -4,4 +4,13 @@ describe("Check password strength", () => {
     test("Password is very week if provide smaller password", ()=>{
         expect(calculatePasswordStrength("asd123")).toBe("Very Weak")
     })
+    test("Password is week if provide minimum password requirements", ()=>{
+        expect(calculatePasswordStrength("asdgh1238")).toBe("Weak")
+    })
+    test("Password is moderate if provide average password requirements", ()=>{
+        expect(calculatePasswordStrength("ASDFG12384454")).toBe("Moderate")
+    })
+    test("Password is strong if provide all password requirements", ()=>{
+        expect(calculatePasswordStrength("asd&%/gh1238")).toBe("Strong")
+    })
 })
